42dbae67c67edf1032b3d681a03fcbb3899b7100
Jose Badeau <jose.badeau@gmail.com>
2017-04-18 09:51:54 +0200

    initial contribution

---
+++ jhipster-scout-generator/README.md
@@ -0,0 +1,57 @@
+# generator-jhipster-generator-jhipster-eclipse-scout
+[![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> JHipster module, JHipster module for creating an Eclipse Scout application.
+
+# Introduction
+
+This is a [JHipster](http://jhipster.github.io/) module, that is meant to be used in a JHipster application.
+
+# Prerequisites
+
+As this is a [JHipster](http://jhipster.github.io/) module, we expect you have JHipster and its related tools already installed:
+
+- [Installing JHipster](https://jhipster.github.io/installation.html)
+
+# Installation
+
+## With Yarn
+
+To install this module:
+
+```bash
+yarn global add generator-jhipster-generator-jhipster-eclipse-scout
+```
+
+To update this module:
+
+```bash
+yarn global upgrade generator-jhipster-generator-jhipster-eclipse-scout
+```
+
+## With NPM
+
+To install this module:
+
+```bash
+npm install -g generator-jhipster-generator-jhipster-eclipse-scout
+```
+
+To update this module:
+
+```bash
+npm update -g generator-jhipster-generator-jhipster-eclipse-scout
+```
+
+# Usage
+
+# License
+
+Apache-2.0 © [ Jose Badeau](jose.badeau@gmail.com)
+
+
+[npm-image]: https://img.shields.io/npm/v/generator-jhipster-generator-jhipster-eclipse-scout.svg
+[npm-url]: https://npmjs.org/package/generator-jhipster-generator-jhipster-eclipse-scout
+[travis-image]: https://travis-ci.org/jbadeau/generator-jhipster-generator-jhipster-eclipse-scout.svg?branch=master
+[travis-url]: https://travis-ci.org/jbadeau/generator-jhipster-generator-jhipster-eclipse-scout
+[daviddm-image]: https://david-dm.org/jbadeau/generator-jhipster-generator-jhipster-eclipse-scout.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/jbadeau/generator-jhipster-module
---
+++ jhipster-scout-generator/generators/entity/index.js
@@ -0,0 +1,96 @@
+const chalk = require('chalk');
+const generator = require('yeoman-generator');
+const packagejs = require(__dirname + '/../../package.json');
+
+// Stores JHipster variables
+const jhipsterVar = {moduleName: 'generator-jhipster-eclipse-scout'};
+
+// Stores JHipster functions
+const jhipsterFunc = {};
+
+module.exports = generator.extend({
+
+    initializing: {
+        compose() {
+            this.entityConfig = this.options.entityConfig;
+            this.composeWith('jhipster:modules', {
+                jhipsterVar: jhipsterVar,
+                jhipsterFunc: jhipsterFunc
+            });
+        },
+        displayLogo() {
+            console.log(chalk.white('Running ' + chalk.bold('JHipster generator-jhipster-eclipse-scout') + ' Generator! ' + chalk.yellow('v' + packagejs.version + '\n')));
+        },
+        validate() {
+            // this shouldnt be run directly
+            if (!this.entityConfig) {
+                this.env.error(chalk.red.bold('ERROR!') + ' This sub generator should be used only from JHipster and cannot be run directly...\n');
+            }
+        }
+    },
+
+    prompting() {
+        // don't prompt if data are imported from a file
+        if (this.entityConfig.useConfigurationFile == true && this.entityConfig.data && typeof this.entityConfig.data.yourOptionKey !== 'undefined') {
+            this.yourOptionKey = this.entityConfig.data.yourOptionKey;
+            return;
+        }
+        const done = this.async();
+        const prompts = [
+            {
+                type: 'confirm',
+                name: 'enableOption',
+                message: 'Some option here?',
+                default: false
+            }
+        ];
+
+        this.prompt(prompts).then((props) => {
+            this.props = props;
+            // To access props later use this.props.someOption;
+
+            done();
+        });
+    },
+
+    writing() {
+        updateFiles() {
+
+            this.baseName = jhipsterVar.baseName;
+            this.packageName = jhipsterVar.packageName;
+            this.angularAppName = jhipsterVar.angularAppName;
+            this.frontendBuilder = jhipsterVar.frontendBuilder;
+
+            var webappDir = jhipsterVar.webappDir,
+            javaTemplateDir = 'src/main/java/package',
+            javaDir = jhipsterVar.javaDir,
+            resourceDir = jhipsterVar.resourceDir,
+            entityName = this.entityConfig.entityClass;
+
+            // do your stuff here
+        },
+
+        writeFiles() {
+            // function to use directly template
+            this.template = function (source, destination) {
+                this.fs.copyTpl(
+                    this.templatePath(source),
+                    this.destinationPath(destination),
+                    this
+                );
+            };
+
+            this.template('dummy.txt', 'dummy.txt', this, {});
+        },
+
+        updateConfig() {
+            jhipsterFunc.updateEntityConfig(this.entityConfig.filename, 'yourOptionKey', this.yourOptionKey);
+        }
+    },
+
+    end() {
+        if (this.yourOptionKey){
+            console.log('\n' + chalk.bold.green('generator-jhipster-eclipse-scout enabled'));
+        }
+    }
+});
